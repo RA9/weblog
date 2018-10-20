ce215ca6abeb5fe9787b39913494908558f30e70
ra9 <cnah27@gmail.com>
2018-10-20 11:49:31 +0000

    Modified files

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,7 +3,7 @@ import CMS from 'netlify-cms'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
-import ProductPagePreview from './preview-templates/ProjectPagePreview'
+import ProjectPagePreview from './preview-templates/ProjectPagePreview'
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
